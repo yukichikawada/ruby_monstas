063372300fd00e57d7d6fb6e22daf8d47ca93e02
Yukichi Kawada <yukichikawada@gmail.com>
2018-06-16 21:50:02 -0700

    Enable sessions and set cookie data

--- monstas.rb
+++ monstas.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'erb'
 
+enable :sessions
+
 get '/' do
   'OMG, hello Ruby Monstas!'
 end
@@ -20,13 +22,26 @@ get '/monstas/:name' do
 end
 
 get '/monstas' do
+  @message = session.delete(:message)
+  @name = params['name']
+  @names = read_names
+  erb :monstas
+end
+
+post '/monstas' do
   @name = params['name']
   write_name_to_file("names.txt", @name)
-  erb :monstas
+  session[:message] = "Succesfully saved the name #{@name}."
+  redirect "/monstas?name=#{@name}"
 end
 
 def write_name_to_file(filename, arg)
   File.open('names.txt', 'a+') do |file|
     file.puts(@name)
   end
+end
+
+def read_names
+  return [] unless File.exists?('names.txt')
+  File.read('names.txt').split("\n")
 end
