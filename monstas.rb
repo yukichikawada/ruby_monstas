894ed74bb905839c199f6ab4209e12525b1fb90c
Yukichi Kawada <yukichikawada@gmail.com>
2018-06-16 19:24:10 -0700

    Add names to external file and gitignore

--- monstas.rb
+++ monstas.rb
@@ -15,5 +15,18 @@ post '/signin' do
 end
 
 get '/monstas/:name' do
-  erb :monstas, { :locals => params }
+  @name = params['name']
+  erb :monstas
+end
+
+get '/monstas' do
+  @name = params['name']
+  write_name_to_file("names.txt", @name)
+  erb :monstas
+end
+
+def write_name_to_file(filename, arg)
+  File.open('names.txt', 'a+') do |file|
+    file.puts(@name)
+  end
 end
